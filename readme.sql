205a05da174682e8382108b0a6d260a013ebe871
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-10-17 09:58:17 +0200

    Initial commit

---
+++ readme.sql
@@ -0,0 +1,303 @@
+# Schéma école
+
+## Écrire les requêtes SQL permettant de répondre aux questions suivantes :
+
+### 1. Select simple
+
+#Dans les requêtes suivantes, on cherche à afficher toutes les colonnes et toutes les lignes de la table.
+
+#1. Afficher la liste des élèves.
+
+
+#2. Afficher la liste des élèves, sans afficher plusieurs fois le même nom de famille.
+
+
+
+#3. Afficher la liste des résultats.
+
+
+
+#4. Afficher la liste des cours.
+
+
+
+### 2. Projection
+
+#Dans les requêtes suivantes, on cherche à afficher seulement certaines colonnes et toutes les lignes.
+
+#1. Afficher la liste des noms et prénoms des élèves.
+
+
+
+#2. Afficher la liste des noms et prénoms des élèves sans doublons.
+
+
+
+#3. Afficher la liste des noms et le salaire des professeurs.
+
+
+
+#4. Afficher la liste des noms des cours.
+
+
+
+### 3. Sélection avec critère simple
+
+#Dans les requêtes suivantes, on cherche à afficher toutes colonnes et seulement certaines lignes.
+
+#1. Afficher la liste des élèves de 1er année.
+
+
+
+#2. Afficher la liste des élèves qui ne sont pas en 1re année. (Proposez 5 manières différentes)
+
+
+
+#3. Afficher la liste des élèves dont le nom est 'Dupont'.
+
+
+
+#4. Afficher la liste des élèves qui passent plus de 7 heures devant leur écran.
+
+
+
+#5. Afficher la liste des élèves dont le temps d'écrans est compris entre 6 et 10 heures.
+
+
+
+#6. Afficher la liste des activités dont le nom d’équipe commence par 'CodeLab'.
+
+
+
+#7. Afficher la liste des activités dont le nom d’équipe contient la lettre 'o' en avant-dernière position.
+
+
+
+#8. Afficher la liste des professeurs dont le nom se termine par la lettre 's'.
+
+
+
+#9. Afficher la liste des activités dont le nom d’équipe contient au moins un espace.
+
+
+
+#10. Afficher la liste des professeurs dont la spécialité est Bases de données ou Développement mobile.
+
+
+
+#11. Afficher la liste des élèves dont le temps d'écran est une des valeurs suivantes : 7.5, 9, 6.8, 10.5, 6.2.
+
+
+
+#12. Afficher la liste des spécialités (qui ne sont pas null).
+
+
+
+#13. Afficher la liste des professeurs qui n’ont jamais reçu de promotion.
+
+
+
+### 4. Sélection avec critères multiples
+
+#Dans les requêtes suivantes, on cherche à afficher toutes les colonnes et seulement certaines lignes.
+
+#1. Afficher la liste des élèves qui passent plus de 7 heures devant des écrans et inscrit en première année.
+
+
+
+#2. Afficher la liste des professeurs dont le salaire de base est de 3800.00 et dont la spécialité est 'Développement Web'.
+
+
+
+#3. Afficher la liste des cours de 1er année qui ont une durée de 30h.
+
+
+
+### 5. Fonctions de calcul
+
+#Dans les requêtes suivantes, on cherche à compter, à calculer des sommes, des moyennes, etc.
+
+#1. Obtenir le temps d'écran moyen des élèves.
+
+
+
+#2. Obtenir le temps d'écran moyen des élèves de 1er année.
+
+
+
+#3. Obtenir le temps d'écran le plus élevé des élèves de 2e année.
+
+
+
+#4. Obtenir le nombre d’élèves de 1er année.
+
+
+#5. Obtenir le nombre d’élèves total.
+
+
+
+#6. Obtenir la somme des salaires de base des professeurs.
+
+
+
+#7. Obtenir le plus bas des salaires de base des professeurs.
+
+
+
+#8. Obtenir le temps d'écran le plus élevé des élèves de 3e année.
+
+
+
+#9. Obtenir le nombre de spécialités.
+
+
+
+#10. Obtenir le nombre de spécialités différentes.
+
+
+
+#11. Obtenir le nombre de professeurs.
+
+
+
+#12. Compter le nombre de fois que la spécialité 'Bases de données' est attribuée.
+
+
+
+#13. Obtenir le temps d'écran total des élèves de 2e année.
+
+
+
+### 6. Opérations arithmétiques
+
+#1. Obtenir le nom et salaire mensuel actuel des profs.
+
+
+
+#2. Calculer l’augmentation du salaire des profs.
+
+
+
+#3. Calculer l’augmentation du salaire mensuel des profs.
+
+
+
+#4. Obtenir le nom des profs dont l’augmentation du salaire de base dépasse 28%.
+
+
+
+#5. Obtenir l’augmentation mensuelle moyenne des profs.
+
+
+
+### 7. Opérations sur les dates
+
+#1. Afficher la date courante.
+
+
+
+#2. Afficher la liste des élèves dont la date de naissance est inférieure au 27/02/2008.
+
+
+
+# 3. Afficher la liste des profs dont l’ancienneté est d’au moins 10 ans.
+
+
+
+### 8. Requêtes imbriquées
+
+# 1. Afficher le nom et prénom des élèves dont le temps d'écran est supérieur à celui de "Coppens Mathis".
+
+
+
+# 2. Obtenir la plus petite note de l’élève "Rossi".
+
+
+
+### 9. Jointure
+
+# 1. Afficher la liste des élèves qui participe à l'activité 'Création graphique' au niveau 1.
+
+
+
+#2. Obtenir le nom des élèves de l’équipe 'Pixel Squad' indus.
+
+
+
+#3. Afficher les points de "Dupont Léa" obtenus dans chaque cours.
+
+
+
+#4. Afficher les points de "Dupont Léa" obtenus dans chaque cours sur 100 plutôt que sur 20.
+
+
+
+#5. Obtenir la plus petite note de l’élève "Mertens".
+
+
+
+#6. Obtenir la plus petite note et le nom de ce cours pour l’élève "Mertens".
+
+
+
+### 10. Tri
+
+#1. Afficher par ordre alphabétique le nom et en ordre inverse le prénom des élèves.
+
+
+
+#2. Afficher la liste des profs par ordre décroissant de salaire.
+
+
+
+#3. Afficher les points de "Mertens" obtenus dans chaque cours sur 100 plutôt que sur 20, triés par points.
+
+
+
+### 11. Groupement de lignes
+
+#1. Obtenir pour chaque élève de 1er année son nom et sa moyenne.
+
+
+
+#2. Obtenir le maximum parmi les totaux de chaque étudiant.
+
+
+
+### 12. Ajouter
+
+#1. Ajouter un nouvel élève : "Dujardin, Jean, 29 mars 2006, 8 - 2".
+
+
+
+#2. Ajouter un nouveau professeur : "Tournesol, Inventeur, 01/09/2002, 01/09/2025, 100000 - 1000000".
+
+
+
+#3. Ajouter les cours de : "Fusée, 150, 2" et "Astronaute, 200, 2". Ces deux nouveaux cours sont à la charge de "Tournesol".
+
+
+
+### 13. Modification
+
+#1. Modifier la date de naissance de "Dupont Léa" par 01/12/2008.
+
+
+
+#2. Remplacer la spécialité "Gestion de projet" par "Entrepreneuriat".
+
+
+
+#3. Corriger le nom 'Dupont Léa' en 'Dupont Sophie'.
+
+
+
+### 14. Suppression
+
+#1. Supprimer les résultats de "Dumont Alexis".
+
+
+
+#2. Supprimer le cours de "Gestion de projet".
+
+
